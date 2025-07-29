14dff5e2fdbb460bef401bcdd06551e0c7fb0c7c
hariharan152578 <hariharan152578@gmail.com>
2025-07-29 22:18:22 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -17,7 +18,8 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+<BrowserRouter basename="/react-jsonserver-shop">
+  <App />
+</BrowserRouter>
+
 );
